fce3fbaeb0ccbf3e2a5624ece3047ee6183537a6
Luca Lorenzini <chielorenz@gmail.com>
2021-09-18 15:51:51 +0200

    Add custom font and return dynamic response

--- netlify/functions/kagata.js
+++ netlify/functions/kagata.js
@@ -1,4 +1,6 @@
 exports.handler = async (event, context) => {
+    const sentence = event.queryStringParameters.sentence || "Hello, I'm kagata";
+
     let response = `
 <!DOCTYPE html>
 <html lang="en">
@@ -6,10 +8,16 @@ exports.handler = async (event, context) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Kagata</title>
+    <style>
+            @font-face { font-family: ElectroHarmonix; src: url('https://raw.githubusercontent.com/b1n01/kagata/main/electroharmonix.regular.ttf'); } 
+        html, body {
+            font-family: ElectroHarmonix;
+        }
+    </style>
 </head>
 <body>
-    <h1>Hello, I'm kagata</h1>
+    <h1>${sentence}</h1>
 </body>
 </html>
     `;
