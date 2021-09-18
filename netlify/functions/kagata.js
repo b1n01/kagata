607770b677fbc223b189cdf6823d6b2af6f9738c
Luca Lorenzini <chielorenz@gmail.com>
2021-09-18 16:39:24 +0200

    Use correct path with the name of the function

--- netlify/functions/kagata.js
+++ netlify/functions/kagata.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require("path");
 
 // @see https://answers.netlify.com/t/hosting-a-file-along-with-my-function/1527/21
-const fileName = "./body.html";
+const fileName = "./kagata/body.html";
 const resolved = (process.env.LAMBDA_TASK_ROOT) ? path.resolve(process.env.LAMBDA_TASK_ROOT, fileName) : path.resolve(__dirname, fileName);
 let body = fs.readFileSync(resolved);
 
