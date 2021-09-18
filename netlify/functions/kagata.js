fef9bc1e7a68ec6025898122c9eaa5a4b40950ab
Luca Lorenzini <chielorenz@gmail.com>
2021-09-18 16:24:53 +0200

    Debug missing file

--- netlify/functions/kagata.js
+++ netlify/functions/kagata.js
@@ -8,6 +8,6 @@ exports.handler = async (event, context) => {
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/html" },
-        body: dody
+        body: __dirname
     };
 };
