e896544df59410343cf00caeebee8b4e9cb74aba
Luca Lorenzini <chielorenz@gmail.com>
2021-09-18 16:37:23 +0200

    Try LAMBDA_TASK_ROOT

--- netlify/functions/kagata.js
+++ netlify/functions/kagata.js
@@ -1,6 +1,10 @@
-var fs = require('fs');
-let body = fs.readFileSync(require.resolve('./body.html'));
-// let body = '';
+const fs = require('fs');
+const path = require("path");
+
+// @see https://answers.netlify.com/t/hosting-a-file-along-with-my-function/1527/21
+const fileName = "./body.html";
+const resolved = (process.env.LAMBDA_TASK_ROOT) ? path.resolve(process.env.LAMBDA_TASK_ROOT, fileName) : path.resolve(__dirname, fileName);
+let body = fs.readFileSync(resolved);
 
 exports.handler = async (event, context) => {
     const sentence = event.queryStringParameters.sentence || "Hello, I'm kagata";
@@ -9,6 +13,6 @@ exports.handler = async (event, context) => {
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/html" },
-        body: __dirname
+        body: body
     };
 };
