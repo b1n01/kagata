703cfae6695cc67502fc750ac17e3a676b5d0b19
Luca Lorenzini <chielorenz@gmail.com>
2021-09-18 16:14:42 +0200

    Move template into a dedicated file; add style

--- netlify/functions/kagata.js
+++ netlify/functions/kagata.js
@@ -1,30 +1,13 @@
+var fs = require('fs');
+let body = fs.readFileSync('body.html');
+
 exports.handler = async (event, context) => {
     const sentence = event.queryStringParameters.sentence || "Hello, I'm kagata";
+    body.replace("{sentence}", sentence);
 
-    let response = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kagata</title>
-    <style>
-            @font-face { font-family: ElectroHarmonix; src: url('https://raw.githubusercontent.com/b1n01/kagata/main/electroharmonix.regular.ttf'); } 
-        html, body {
-            font-family: ElectroHarmonix;
-        }
-    </style>
-</head>
-<body>
-    <h1>${sentence}</h1>
-</body>
-</html>
-    `;
-
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/html"},
-    body: response
-  };
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "text/html" },
+        body: dody
+    };
 };
