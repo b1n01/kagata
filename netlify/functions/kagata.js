851baa85eb01b99cc2ad4f80248fdb30e183d4f0
Luca Lorenzini <chielorenz@gmail.com>
2021-09-18 17:35:35 +0200

    Await response body

--- netlify/functions/kagata.js
+++ netlify/functions/kagata.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 exports.handler = async (event, context) => {
     const sentence = event.queryStringParameters.sentence || "Hello, I'm kagata";
     const response = await fetch('https://raw.githubusercontent.com/b1n01/kagata/main/body.html');
-    let body = response.body;
+    let body = await response.body;
     body = body.replace("{sentence}", sentence);
 
     return {
