8b2c8ae7a3dc2eb43d23ec387689f9c0103af300
Luca Lorenzini <chielorenz@gmail.com>
2021-09-20 14:07:23 +0200

    Add netlify-cli and init svelte project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Kagata!!!
+
+## Up and running
+
+```
+npm i
+npm install netlify-cli -g
+netlify login
+netlify dev
+```
--- functions/kagata.js
+++ functions/kagata.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 
 exports.handler = async (event, context) => {
-    const sentence = event.queryStringParameters.sentence || "Hello, I'm kagata";
-    const response = await fetch('https://raw.githubusercontent.com/b1n01/kagata/main/body.html');
+    const content = event.queryStringParameters.c || "Hello, I'm kagata";
+    const response = await fetch('https://raw.githubusercontent.com/b1n01/kagata/main/assets/body.html');
     let body = await response.text();
-    body = body.replace("{sentence}", sentence);
+    body = body.replace("{content}", content);
 
     return {
         statusCode: 200,
