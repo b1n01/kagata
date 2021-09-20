1eaa965550d8a95daf671f5c41734ec774537bd6
Luca Lorenzini <chielorenz@gmail.com>
2021-09-20 14:17:35 +0200

    Use aessets served from netlify

--- functions/kagata.js
+++ functions/kagata.js
@@ -2,7 +2,8 @@ import fetch from 'node-fetch';
 
 exports.handler = async (event, context) => {
     const content = event.queryStringParameters.c || "Hello, I'm kagata";
-    const response = await fetch('https://raw.githubusercontent.com/b1n01/kagata/main/assets/body.html');
+    // TODO how to fetch body.html locally?
+    const response = await fetch('https://kagata.netlify.app/assets/body.html');
     let body = await response.text();
     body = body.replace("{content}", content);
 
