edc7d7c7264ed248ef8320a8dd2a4cc5852d0a44
Luca Lorenzini <chielorenz@gmail.com>
2021-09-23 21:30:13 +0200

    Updaye body.html style

--- functions/kagata.js
+++ functions/kagata.js
@@ -1,11 +1,11 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
     const content = event.queryStringParameters.c || "Hello, I'm kagata";
     const author = event.queryStringParameters.a || "Kagata";
-    const date = new Date().toLocaleDateString('it-IT', {year: 'numeric', month: 'long', day: 'numeric' });
+    const date = new Date().toLocaleDateString("it-IT", {year: "numeric", month: "long", day: "numeric" });
 
-    const response = await fetch(process.env.ENDPOINT + '/body.html');
+    const response = await fetch(process.env.ENDPOINT + "/body.html");
     let body = await response.text();
 
     body = body.replace("{content}", content);
